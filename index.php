38ccfe80dec1d34349d5c47fc6937e1525251c14
Spyrow <Spiip76@gmail.com>
2014-01-09 18:46:27 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,65 @@
+<!-- ----------------------------------------------------------- -->
+<!-- Site développé et design réalisé par AyPhun pour DRAKITFR -->
+<!-- Twitter d'AyPhun : https://twitter.com/LouisScarpitta -->
+<!-- Twitter de DrakitFR : https://twitter.com/DrakitFR -->
+
+<!-- Copyright 2014 -->
+
+
+<!doctype html>
+<html lang="en">
+<head>
+	<title>DrakitFR #OFF</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no" />
+    <link rel="stylesheet" type="text/css" href="css/normalize.css"/>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="css/bebasneue/css/bebasneue.css"/>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/slicknav.css">
+	<link rel="stylesheet" type="text/css" href="css/responsive.css"/>
+
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
+	<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+	<script src="js/jquery.slicknav.min.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
+
+</head>
+<body>
+	<div id="page-404-1" class="panda-wrapper panda-404">
+		<div class="panda-content page-404">
+			<header>
+				<div class="dot"></div>
+				<div class="logo-pd-404"><img src="img/logo-pd-404.png" alt=""></div>
+				<div class="logo-panda"><img src="img/drakitfr.png" alt=""></div>
+			</header>
+			<h2 class="rs title-404">Oh Wait, c'est fermé !</h2>
+			<p class="rs">Le site de @DrakitFR est fermé. <br />Mon serveur : <code>drakit.n-minecraft.com</code></p>
+			<div class="panda-bg-arrow"></div>
+
+			<div id="responsivemenu"></div>
+			<nav class="panda-menu">
+				<ul id="menu" class="rs">
+					<li class="current"><a href="#">DrakitFR</a></li>
+					<li><a href="#">DrakitFR</a></li>
+					<li><a href="#">DrakitFR</a></li>
+					<li><a href="#">DrakitFR</a></li>
+					<li><a href="#">DrakitFR</a></li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+
+<footer>
+<center><p>test</p></center>
+</footer>
+	
+</body>
+</html>
+
+<!-- Copyright 2014 -->
+
+<!-- Twitter d'AyPhun : https://twitter.com/LouisScarpitta -->
+<!-- Twitter de DrakitFR : https://twitter.com/DrakitFR -->
+<!-- Site développé et design réalisé par AyPhun pour DRAKITFR -->
+<!-- ----------------------------------------------------------- -->
